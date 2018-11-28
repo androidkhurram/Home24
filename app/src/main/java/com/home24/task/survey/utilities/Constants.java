13c06ae8fb244dbed78dbe01de6565ec70c3810d
Khurram <edesign724@gmail.com>
2018-11-28 09:19:29 -0600

    Minor code cleaning

--- app/src/main/java/com/home24/task/survey/utilities/Constants.java
+++ app/src/main/java/com/home24/task/survey/utilities/Constants.java
@@ -2,26 +2,10 @@ package com.home24.task.survey.utilities;
 
 public class Constants
 {
-    public static final String DEVICE_TYPE_IPHONE  = "iPhone";
-    public static final String DEVICE_TYPE_ANDROID = "Android";
-    public static int IMAGE_SIZE = 800;
 
-    public static boolean isSandBox = false;
+
     public static boolean isDebug   = true;
 
-    public static final int RESULT_COUNTRY    = 9000;
-    public static final int RESULT_WISHLIST = 10919;
-
-    /**************************************************
-     *
-     *		 RESULT CODES
-     **************************************************/
-    public static final String IMAGE_DIRECTORY = "images";
-    public static final String AUDIO_DIRECTORY = "audio";
-
-
-    public static final String TEMP_DIRECTORY = "temp";
-    public static final String VIDEO_DIRECTORY = "video";
 
     /**************************************************
 	 *
@@ -32,39 +16,10 @@ public class Constants
     public static final String USERDEFAULT_ISLOGGEDIN   = "isLoggedIn";
     public static final String USERDEFAULT_STORE_DATA   = "storeData";
     public static final String USERDEFAULT_USER_DATA    = "userData";
-    public static final String USERDEFAULT_WISH_LIST   = "wishList";
-    public static final String USERDEFAULT_WISH_LIST_DIAM   = "diamondWishList";
     public static final String USERDEFAULT_REGISTER_COUNTER   = "registerCounter";
 
     public static final String USERDEFAULT_CLOSEST_STORE  = "hasFoundClosestStore";
     public static final String USERDEFAULT_STORE_ALERTED  = "storeAlerted";
-
-    /**************************************************
-     *
-     *		 RESPONSE KEYS
-     *
-     **************************************************/
-    public static final String RESPONSE_KEY_ERROR        = "error";
-    public static final String RESPONSE_KEY_MESSAGE      = "message";
-    public static final String RESPONSE_KEY_STORE        = "store";
-    public static final String RESPONSE_KEY_INVALID_KEY  = "invalidKey";
-    public static final String RESPONSE_KEY_FOUND_STORE  = "hasFoundClosestStore";
-    public static final String RESPONSE_KEY_DATA        = "data";
-
-    public static final String RESPONSE_KEY_USER        = "user";
-    public static final String RESPONSE_KEY_MOOD        = "mood";
-
-    /**************************************************
-     *
-     *
-     *
-     **************************************************/
-    public static final String ACTION_MSG_RECEIVED       = "messageReceived";
-    public static final String ACTION_TYPING             = "actionTyping";
-    public static final String ACTION_SOCKET_DISCONNECT     = "socketDisconnect";
-    public static final String ACTION_MSG_NOTIF             = "messageNotif";
-
-    public static final int PUSH_ID = 900;
 
 
     /**************************************************
